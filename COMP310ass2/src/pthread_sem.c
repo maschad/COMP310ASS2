baa80deb281cc1dfa2c409c698c2ddd195799411
cnehem <cnehem@open-18.CS.McGill.CA>
2015-02-21 17:28:57 -0500

    second committ sleep working

--- COMP310ass2/src/pthread_sem.c
+++ COMP310ass2/src/pthread_sem.c
@@ -1,65 +1,70 @@
 #include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 #include <semaphore.h>
 #include <pthread.h>
 
 
 static int glob = 0;
-static sem_t sem,r,w;//inital vals of 1
+static sem_t sem,r,w;//Initial vals of 1
+struct timespec tim, tim2;//Sleep variables
 
+static void *readFunc(void *arg)
+{
+	int loc, j,z;
+	tim.tv_sec = 1;
+	tim.tv_nsec = 500;
 
-static void *threadFunc(void *arg) {
+	for (j = 0; j < 100; j++)
+	{
 
-  int loops = *((int *) arg);
-  int loc, j;
-
-  for (j = 0; j < loops; j++) {
-
-     if (sem_wait(&sem) == -1)
-       exit(2);
-
-    loc = glob;
-    loc++;
-    glob = loc;
-
-        if (sem_post(&sem) == -1)
-         exit(2);
-  }
-  return NULL;
+		if (sem_wait(&sem) == -1)
+			exit(2);
+		glob = loc;
+		z = nanosleep(&tim,&tim2);
+		printf("\n Value at shared %i",glob);
+		if (sem_post(&sem) == -1)
+			exit(2);
+	}
+	  return NULL;
 }
 
+static void *writerFunc(void *arg)
+{
+	  int loc, j;
 
+	  for (j = 0; j < 10; j++)
+	  {
+	     if (sem_wait(&sem) == -1)
+	       exit(2);
+
+	    loc = glob;
+	    loc+=10;
+	    glob = loc;
+		if (sem_post(&sem) == -1)
+			exit(2);
+	  }
+	  return NULL;
+}
 
 int main(int argc, char *argv[]) {
 
-  pthread_t t1, t2, t3, t4;
-  int s;
-  int loops = 100000;
+  pthread_t t1, t2;
+  int s;/*indicates if successful thread creation or not*/
+
 
   if (sem_init(&sem, 0, 1) == -1) {
     printf("Error, init semaphore\n");
     exit(1);
   }
 
-  s = pthread_create(&t1, NULL, threadFunc, &loops);
+  s = pthread_create(&t1, NULL, writerFunc,0);
   if (s != 0) {
     printf("Error, creating threads\n");
     exit(1);
   }
 
-  s = pthread_create(&t2, NULL, threadFunc, &loops);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  s = pthread_create(&t3, NULL, threadFunc, &loops);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  s = pthread_create(&t4, NULL, threadFunc, &loops);
+  s = pthread_create(&t2, NULL, readFunc,0);
   if (s != 0) {
     printf("Error, creating threads\n");
     exit(1);
@@ -77,51 +82,6 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  s = pthread_join(t3, NULL);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  s = pthread_join(t4, NULL);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
-
-  printf("glob value %d \n", glob);
   exit(0);
 }
 
-
-void reader()/*Reader*/
-{
-	wait(r);
-	wait(sem);
-	glob++;
-	if (glob == 1)
-	  wait(w);
-	signal(sem);
-	signal(r);
-
-        // reading is performed
-
-	wait(sem);
-	glob--;
-	if (glob == 0)
-		signal(w);
-	signal(sem);
-}
-
-
-void writer()//WRITER
-{
-
-    wait(r);
-    wait(w);
-
-     // writing is performed
-
-    signal(w);
-    signal(r);
-}
