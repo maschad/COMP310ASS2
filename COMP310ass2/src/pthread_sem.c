c35d16ccf942c22ce8ba3f46ca7b707fcccbb9b3
cnehem <cnehem@open-18.CS.McGill.CA>
2015-02-21 19:40:54 -0500

    third committ not sharing semaphore properly tho

--- COMP310ass2/src/pthread_sem.c
+++ COMP310ass2/src/pthread_sem.c
@@ -5,71 +5,71 @@
 #include <pthread.h>
 
 
-static int glob = 0;
-static sem_t sem,r,w;//Initial vals of 1
+static int glob = 0;// globally shared resource
+int read_count = 0;// Keeps track of how threads are currently reading the object
+static sem_t rw_mutex;// common to both reader and writer threads
+static sem_t mutex;//Initial vals of 1
 struct timespec tim, tim2;//Sleep variables
 
-static void *readFunc(void *arg)
+void static writer(pthread_t thread)
 {
-	int loc, j,z;
-	tim.tv_sec = 1;
-	tim.tv_nsec = 500;
+	int loc;
 
-	for (j = 0; j < 100; j++)
-	{
-
-		if (sem_wait(&sem) == -1)
-			exit(2);
-		glob = loc;
-		z = nanosleep(&tim,&tim2);
-		printf("\n Value at shared %i",glob);
-		if (sem_post(&sem) == -1)
-			exit(2);
-	}
-	  return NULL;
+	wait(rw_mutex);
+	loc = glob;
+	loc+= 10;
+	glob = loc;
+	signal(rw_mutex);
 }
 
-static void *writerFunc(void *arg)
+void static read(pthread_t thread)
 {
-	  int loc, j;
-
-	  for (j = 0; j < 10; j++)
-	  {
-	     if (sem_wait(&sem) == -1)
-	       exit(2);
-
-	    loc = glob;
-	    loc+=10;
-	    glob = loc;
-		if (sem_post(&sem) == -1)
-			exit(2);
-	  }
-	  return NULL;
+	wait(mutex);
+	read_count++;
+	if (read_count == 1)
+	wait(rw_mutex);
+	signal(mutex);
+	printf("\nValue of glob %i",glob);
+	wait(mutex);
+	read_count --;
+	if (read_count == 0)
+		signal(rw_mutex);
+	signal(mutex);
 }
 
 int main(int argc, char *argv[]) {
 
   pthread_t t1, t2;
   int s;/*indicates if successful thread creation or not*/
+  int loops;
 
 
-  if (sem_init(&sem, 0, 1) == -1) {
+  if (sem_init(&rw_mutex, 1, 1) == -1) {
     printf("Error, init semaphore\n");
     exit(1);
   }
 
-  s = pthread_create(&t1, NULL, writerFunc,0);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
-  }
+  if(sem_init(&mutex,1,1) == -1){
+	    printf("Error, init semaphore\n");
+	    exit(1);
+	  }
 
-  s = pthread_create(&t2, NULL, readFunc,0);
-  if (s != 0) {
-    printf("Error, creating threads\n");
-    exit(1);
+  for(loops = 0; loops < 10;loops++)
+  {
+	  s = pthread_create(&t1, NULL, writer,0);
+	  if (s != 0) {
+		printf("Error, creating threads\n");
+		exit(1);
+	  }
   }
-
+  for(loops = 0; loops < 100;loops++)
+  {
+	  s = pthread_create(&t2, NULL,read,0);
+	  if (s != 0) {
+		printf("Error, creating threads\n");
+		exit(1);
+	  }
+  }
   s = pthread_join(t1, NULL);
   if (s != 0) {
     printf("Error, creating threads\n");
