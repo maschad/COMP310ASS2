c628a8f9129089ba06604879f590d506305fd5b4
cnehem <cnehem@open-18.CS.McGill.CA>
2015-02-21 22:17:04 -0500

    Have something working but need to measure times

--- COMP310ass2/src/pthread_sem.c
+++ COMP310ass2/src/pthread_sem.c
@@ -11,38 +11,59 @@ static sem_t rw_mutex;// common to both reader and writer threads
 static sem_t mutex;//Initial vals of 1
 struct timespec tim, tim2;//Sleep variables
 
-void static writer(pthread_t thread)
+void static writer(void *args)
 {
 	int loc;
 
-	wait(rw_mutex);
-	loc = glob;
-	loc+= 10;
-	glob = loc;
-	signal(rw_mutex);
+	sem_wait(&rw_mutex);
+	glob+=10;
+	sem_post(&rw_mutex);
+	pthread_exit(NULL);
 }
 
-void static read(pthread_t thread)
+void static reader(void *args)
 {
-	wait(mutex);
-	read_count++;
-	if (read_count == 1)
-	wait(rw_mutex);
-	signal(mutex);
-	printf("\nValue of glob %i",glob);
-	wait(mutex);
-	read_count --;
-	if (read_count == 0)
-		signal(rw_mutex);
-	signal(mutex);
+	int tries = *((int *) args);
+	int j = 0;
+	do
+	{
+		if(sem_wait(&mutex) == -1)
+		{
+			exit(2);
+		}
+		read_count++;
+		if (read_count == 1)
+		{
+			if(sem_wait(&rw_mutex) == -1)
+			{
+					exit(2);
+			}
+		}
+		if(sem_post(&mutex) == -1)
+		{
+			exit(2);
+		}
+		printf("\nValue of glob %i",glob);
+		if(sem_wait(&mutex) == -1)
+		{
+			exit(2);
+		}
+		read_count --;
+		if (read_count == 0)
+		{
+			sem_wait(&rw_mutex);
+		}
+		sem_post(&mutex);
+		j++;
+	}while(j < tries);
+	pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
-
   pthread_t t1, t2;
   int s;/*indicates if successful thread creation or not*/
-  int loops;
-
+  int loops;/* amount of loops for a thread*/
+  int tries = 10; /*amount of tries to do*/
 
   if (sem_init(&rw_mutex, 1, 1) == -1) {
     printf("Error, init semaphore\n");
@@ -56,7 +77,7 @@ int main(int argc, char *argv[]) {
 
   for(loops = 0; loops < 10;loops++)
   {
-	  s = pthread_create(&t1, NULL, writer,0);
+	  s = pthread_create(&t1, NULL, writer,&tries);
 	  if (s != 0) {
 		printf("Error, creating threads\n");
 		exit(1);
@@ -64,7 +85,7 @@ int main(int argc, char *argv[]) {
   }
   for(loops = 0; loops < 100;loops++)
   {
-	  s = pthread_create(&t2, NULL,read,0);
+	  s = pthread_create(&t2, NULL,reader,&tries);
 	  if (s != 0) {
 		printf("Error, creating threads\n");
 		exit(1);
